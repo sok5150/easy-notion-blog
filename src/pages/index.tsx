ef3cc00258a561b0209403e54ba5337746620e9b
sok41 <lemoned.rhcp@gmail.com>
2022-10-10 17:56:38 +0900

    modified index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ import {
   getRankedPosts,
   getAllTags,
 } from '../lib/notion/client'
-import Image from 'next/image'
+// import Image from 'next/image'
 import Myprofile from './myprofile'
 
 export async function getStaticProps() {
